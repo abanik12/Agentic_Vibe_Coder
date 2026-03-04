08bf796b589c91f5a0cfbdbc284436474eba48ab
abanik12 <arijit.bnk@gmail.com>
2026-03-03 17:37:32 -0800

    addec changes for voice output

---
+++ app/graph.py
@@ -0,0 +1,59 @@
+import os
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.graph import StateGraph, START, END
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage
+
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+@tool
+def exec_command(cmd: str):
+    """
+    Takes a command line prompt and executes it on the user's machine and 
+    returns the output of the command.
+    Example: run_command(cmd="mkdir folder_name") where mkdir creates a directory/folder under the current working directory with name as folder_name.
+    """
+    result = os.system(command=cmd)
+    return result
+
+llm = init_chat_model(model_provider="openai", model="gpt-4.1-mini", max_tokens=2048)
+llm_with_tool = llm.bind_tools(tools=[exec_command])
+
+def chatbot(state: State):
+    system_prompt = SystemMessage(content="""
+        You are a helpful AI Coding assistant aka Vibe Coder who takes an input from user and based on available
+        tools you choose the correct tool and execute the commands.
+                                  
+        You can even execute commands and help user with the output of the command.
+
+        Always make sure to keep your generated codes and files in chat_llm/ folder. you can create one if not already there.                           
+    """)
+
+    message = llm_with_tool.invoke([system_prompt] + state["messages"])
+    assert len(message.tool_calls) <= 1
+    return {"messages": [message]}
+
+tool_node = ToolNode(tools=[exec_command])
+
+graph_builder = StateGraph(State)
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+def create_chat_graph(checkpointer):
+    return graph_builder.compile(checkpointer=checkpointer)
